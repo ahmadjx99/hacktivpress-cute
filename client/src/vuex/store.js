bbf03393ec4a539f78868b003910a1f109f6dcbe
budhip <ryo.bunraku@yahoo.com>
2017-08-15 16:30:26 +0700

    create store in folder vuex

---
+++ client/src/vuex/store.js
@@ -0,0 +1,54 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import axios from 'axios'
+
+Vue.use(Vuex)
+
+export const store = new Vuex.Store({
+  state: {
+    articles: [],
+    user: '',
+    searches: []
+  },
+  mutations: {
+    getArticles (state, payload) {
+      state.articles = payload
+    },
+    getUser (state, payload) {
+      state.user = payload
+    },
+    getSearches (state, payload) {
+      state.searches = payload
+    },
+    logout (state) {
+      state.user = ''
+    }
+  },
+  actions: {
+    getArticles (store) {
+      axios.get('http://localhost:3000/api/articles')
+      .then(response => {
+        store.commit('getArticles', response.data)
+      })
+    },
+    getUser (store) {
+      axios.get('http://localhost:3000/userdata', {
+        token: localStorage.getItem('token')
+      })
+      .then(response => {
+        store.commit('getUser', response.data)
+      })
+    }
+  },
+  getters: {
+    articles (state) {
+      return state.articles
+    },
+    user (state) {
+      return state.user
+    },
+    searches (state) {
+      return state.searches
+    }
+  }
+})
