7ddbeeca77a905904c7f45c90c7818bf29439daf
budhip <ryo.bunraku@yahoo.com>
2017-08-15 15:21:13 +0700

    component articles, articleDetail, articlelist

---
+++ client/src/components/ArticleDetail.vue
@@ -0,0 +1,76 @@
+<template lang="html">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-9">
+        <h2>{{ article.title }}</h2>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-12">
+        <p class="content" v-for="content in article.articleContent.split('\n')">{{ content }}</p>
+        <p>Author: {{ article.author.username }}</p>
+        <p>{{ article.category }}</p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-10"></div>
+      <div class="col-2" v-if="user._id === article.author._id">
+        <i class="fa fa-trash buttonme" aria-hidden="true" @click="doRemove" style="color: red"></i>
+        <i class="fa fa-pencil-square-o buttonme" aria-hidden="true" @click="editArticle"></i>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      article: {}
+    }
+  },
+  methods: {
+    getArticle () {
+      let id = this.$route.query
+      var self = this
+      this.axios.get(`http://localhost:3000/api/articles/${id}`)
+      .then(response => {
+        self.article = response.data
+      })
+    },
+    doRemove () {
+      var self = this
+      let id = self.$route.query.id
+      if (window.confirm('Are you sure want to delete?')) {
+        this.axios.delete(`http://localhost:3000/api/articles/${id}`, {
+          token: localStorage.getItem('token')
+        })
+        .then(response => {
+          console.log(response)
+          self.$store.dispatch('getArticles')
+          self.$router.push('/')
+        })
+      }
+    },
+    editArticle () {
+
+    }
+  },
+  computed: {
+    user () {
+      return this.$store.getters.user
+    }
+  },
+  created () {
+    this.getArticle()
+  }
+}
+</script>
+
+<style lang="css">
+.buttonme {
+  font-size: 20px;
+  margin: 0 10px;
+  cursor: pointer;
+}
+</style>
---
+++ client/src/components/ArticleList.vue
@@ -0,0 +1,51 @@
+<template lang="html">
+  <div>
+    <b-list-group>
+      <b-list-group-item>
+        <b-form-input v-model="text" type="text" placeholder="search"></b-form-input><br><br>
+        <b-form-radio v-model="selection":options="options" stacked="success">
+        </b-form-radio>
+        <b-button size="sm" variant="primary" href="" @click="doSearch">
+          Search
+        </b-button>
+      </b-list-group-item>
+      <b-list-group-item v-for="article in articles" :to="{ path: 'article', query: { id: article._id }}" :key="article._id">{{article.title}}</b-list-group-item>
+    </b-list-group>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      text: '',
+      selection: '',
+      options: [{
+        text: 'Search by category',
+        value: 'category'
+      }, {
+        text: 'Search by author',
+        value: 'author'
+      }]
+    }
+  },
+  methods: {
+    doSearch () {
+      var self = this
+      this.axios.get(`http://localhost:3000/api/articles/${self.selection}/${self.text}`)
+      .then(response => {
+        self.$store.commit('getSearches', response.data)
+        self.$router.push('/searchresult')
+      })
+    }
+  },
+  computed: {
+    articles () {
+      return this.$store.getters.articles
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
