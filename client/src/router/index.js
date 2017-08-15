98efaf4a657ffb79fee6a72308bb2815e586bf73
budhip <ryo.bunraku@yahoo.com>
2017-08-15 16:19:17 +0700

    add some components in router/index

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,15 +1,35 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import MainPage from '@/components/MainPage'
+import Signinsignup from '@/components/Signinsignup'
+import CreateArticle from '@/components/CreateArticle'
+import ArticleDetail from '@/components/ArticleDetail'
+import SearchResult from '@/components/SearchResult'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      component: MainPage
+    },
+    {
+      path: '/registration',
+      component: Signinsignup
+    },
+    {
+      path: '/create',
+      component: CreateArticle
+    },
+    {
+      path: '/article',
+      component: ArticleDetail
+    },
+    {
+      path: '/searchresult',
+      component: SearchResult
     }
   ]
 })
